afdeaf578eb4ef1ee34173457be7c3df1ed91de9
Luis Fernando Machado <>
2023-02-14 10:38:43 -0300

    creating and using an Alert Dialog

--- app/src/main/java/com/materialdesign/dialogs/MainActivity.java
+++ app/src/main/java/com/materialdesign/dialogs/MainActivity.java
@@ -1,18 +1,24 @@
 package com.materialdesign.dialogs;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	private Toolbar toolbar;
+	private Button btnAlert;
+	private AlertDialog alertDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,31 @@ public class MainActivity extends AppCompatActivity {
 
 		toolbar = (Toolbar) findViewById(R.id.toolbar_personal);
 		setSupportActionBar(toolbar);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Confirmation");
+		builder.setMessage("Showing Confirmation Alert Dialog");
+		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				Toast.makeText(MainActivity.this, "You has selected Yes option", Toast.LENGTH_SHORT).show();
+			}
+		});
+		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				Toast.makeText(MainActivity.this, "You has selected No option", Toast.LENGTH_SHORT).show();
+			}
+		});
+		alertDialog = builder.create();
+
+		btnAlert = findViewById(R.id.btnAlert);
+		btnAlert.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				alertDialog.show();
+			}
+		});
 	}
 
 	@Override
